5e31342786799f22126c4bb2a10b54291ce6b97f
ryXkci <ricardobozzi2310@gmail.com>
2023-02-10 20:10:35 +0100

    Wrote basic game logicù

--- app.js
+++ app.js
@@ -4,14 +4,45 @@ function getComputerChoice() {
 
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Pick paper, rock or scissor".toLowerCase());
+
 
 
 
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection)
     console.log(computerSelection)
+    let playerScore = 0;
+    let computerScore = 0;
+    if (playerSelection === computerSelection) return `It's a draw! Both ${playerSelection}`
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return `You won! ${playerSelection} beats ${computerSelection}!`
+        }
+        else {
+            return `You lost! ${computerSelection} beats ${playerSelection}!`
+        }
+
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return `You won! ${playerSelection} beats ${computerSelection}!`
+        }
+        else {
+            return `You lost! ${computerSelection} beats ${playerSelection}!`
+        }
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return `You won! ${playerSelection} beats ${computerSelection}!`
+        }
+        else {
+            return `You lost! ${computerSelection} beats ${playerSelection}!`
+        }
+    }
 }
 
+function game(fn) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt("Pick paper, rock or scissor".toLowerCase());
+    return fn(playerSelection, computerSelection)
+}
 /* playRound(playerSelection, computerSelection) */
