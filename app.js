bd8d2e58bdc4a6d6402c1e71c4288aa33d3b8c71
ryXkci <ricardobozzi2310@gmail.com>
2023-02-10 20:58:31 +0100

    Added loop to have five matches and decide winner

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const playerText = document.querySelector('.player');
+const computerText = document.querySelector('.computer');
+
+
 function getComputerChoice() {
     const choices = ['paper', 'rock', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)]
@@ -5,44 +9,81 @@ function getComputerChoice() {
 }
 
 
-
-
-
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection)
     console.log(computerSelection)
-    let playerScore = 0;
-    let computerScore = 0;
     if (playerSelection === computerSelection) return `It's a draw! Both ${playerSelection}`
     if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
+            playerScore++
+            console.log(`***Player score is ${playerScore}***`)
+            console.log(`***Computer Score is ${computerScore}***`)
+            playerText.innerText = playerScore;
+            computerText.innerText = computerScore;
             return `You won! ${playerSelection} beats ${computerSelection}!`
         }
         else {
+            computerScore++
+            console.log(`***Player score is ${playerScore}***`)
+            console.log(`***Computer Score is ${computerScore}***`)
+            playerText.innerText = playerScore;
+            computerText.innerText = computerScore;
             return `You lost! ${computerSelection} beats ${playerSelection}!`
         }
 
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
+            playerScore++
+            console.log(`***Player score is ${playerScore}***`)
+            console.log(`***Computer Score is ${computerScore}***`)
+            playerText.innerText = playerScore;
+            computerText.innerText = computerScore;
             return `You won! ${playerSelection} beats ${computerSelection}!`
         }
         else {
+            computerScore++
+            console.log(`***Player score is ${playerScore}***`)
+            console.log(`***Computer Score is ${computerScore}***`)
+            playerText.innerText = playerScore;
+            computerText.innerText = computerScore;
             return `You lost! ${computerSelection} beats ${playerSelection}!`
         }
     }
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
+            playerScore++
+            console.log(`***Player score is ${playerScore}***`)
+            console.log(`***Computer Score is ${computerScore}***`)
+            playerText.innerText = playerScore;
+            computerText.innerText = computerScore;
             return `You won! ${playerSelection} beats ${computerSelection}!`
         }
         else {
+            computerScore++
+            console.log(`***Player score is ${playerScore}***`)
+            console.log(`***Computer Score is ${computerScore}***`)
+            playerText.innerText = playerScore;
+            computerText.innerText = computerScore;
             return `You lost! ${computerSelection} beats ${playerSelection}!`
         }
     }
 }
 
-function game(fn) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt("Pick paper, rock or scissor".toLowerCase());
-    return fn(playerSelection, computerSelection)
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Pick paper, rock or scissor".toLowerCase());
+        console.log(playRound(playerSelection, computerSelection))
+    }
+    if (playerScore > computerScore) {
+        return `You won the match!`
+    }
+    else {
+        return `You lost the match!`
+    }
 }
 /* playRound(playerSelection, computerSelection) */
